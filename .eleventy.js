da458b489cfb31e5b1051f52b053a154e0f88570
Lara Aydin <ayd.dilara@gmail.com>
2020-12-27 19:25:17 -0800

    Make excerpt work

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const errorOverlay = require("eleventy-plugin-error-overlay")
 const navigation = require("@11ty/eleventy-navigation")
 
-const excerpt = require("./src/filters/excerpt")
+const excerpt = require("eleventy-plugin-excerpt")
 const formatDate = require("./src/filters/formatDate")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(errorOverlay)
   eleventyConfig.addPlugin(navigation)
-  eleventyConfig.addFilter("excerpt", excerpt)
+  eleventyConfig.addPlugin(excerpt)
   eleventyConfig.addFilter("formatDate", formatDate)
   eleventyConfig.addPassthroughCopy("src/images")
   eleventyConfig.addPassthroughCopy("src/js")
